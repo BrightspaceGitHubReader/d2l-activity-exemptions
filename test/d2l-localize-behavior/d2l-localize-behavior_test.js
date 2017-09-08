53ae3a9e3f758c93634d3a5c07b2b2007771fb14
Jared Bydeley <jared.bydeley@d2l.com>
2017-09-08 09:38:51 -0400

    Add tests for d2l-localize-behavior (#28)
    * Add tests for d2l-localize-behavior
    * Refactored tests

---
+++ test/d2l-localize-behavior/d2l-localize-behavior_test.js
@@ -0,0 +1,41 @@
+/*global describe beforeEach it expect fixture*/
+describe('d2l-localize-behavior', function() {
+	let elem;
+
+	beforeEach(function() {
+		document.documentElement.lang = '';
+		document.documentElement.removeAttribute('data-lang-default');
+		elem = fixture('localize-test');
+	});
+
+	[
+		{
+			label: 'English',
+			code: 'en',
+			expected: 'Select / Unselect All'
+		}
+	].forEach(function(val) {
+		it('should render in ' + val.label, function() {
+			document.documentElement.lang = val.code;
+			expect(elem.innerText).to.eql(val.expected);
+		});
+	});
+
+	[
+		'EN',
+		'EN-XX',
+		'',
+		'XX'
+	].forEach( lang => {
+		it(`should fall back to base language when lang is: '${lang}'`, function() {
+			document.documentElement.lang = 'EN';
+			expect(elem.innerText).to.eql('Select / Unselect All');
+		});
+	});
+
+	it('should fall back to org default language', function() {
+		document.documentElement.lang = '';
+		document.documentElement.setAttribute('data-lang-default', 'en');
+		expect(elem.innerText).to.eql('Select / Unselect All');
+	});
+});
