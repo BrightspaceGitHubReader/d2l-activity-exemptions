015ca40b3a4a82825567dfa838ca05caacf4aeb4
BraydonKains <kainsbraydon@gmail.com>
2019-10-11 13:40:53 -0400

    Aria label fix (#63)
    * Updated table to show DisplayName instead of First Name Last Name, set aria label logic for checkboxes
    
    * Didn't actually want to commit the new built languages
    
    * Removed line from previous story I was working on, removed leftover console.log
    
    * Fixed load more button aria label langterm

--- d2l-activity-exemptions.js
+++ d2l-activity-exemptions.js
@@ -112,15 +112,15 @@ class D2LActivityExemptions extends mixinBehaviors(
 			<d2l-tr role="row">
 			  <d2l-th>
 				<d2l-input-checkbox
+				  aria-label$="[[localize('ariaSelectUnselectAll')]]"
 				  id="select-all"
-				  aria-label$="[[localize('selectUnselectAll')]]"
 				  on-change="selectAll"
 				>
 				</d2l-input-checkbox>
 			  </d2l-th>
 
 			  <d2l-th scope="col" role="columnheader" aria-sort="none">
-				[[localize('lblFirstName')]] [[localize('lblLastName')]]
+				[[localize('lblDisplayName')]] 
 			  </d2l-th>
 
 			  <d2l-th scope="col" role="columnheader" aria-sort="none">
@@ -139,13 +139,13 @@ class D2LActivityExemptions extends mixinBehaviors(
 				<d2l-td>
 				  <d2l-input-checkbox
 					class="checkbox-user"
-					aria-label$="[[localize('ariaSelectUser', 'lastName', item.LastName, 'firstName', item.FirstName)]]"
+					aria-label$="[[getCheckboxAriaLabel(item.IsExempt, item.DisplayName)]]"
 				  >
 				  </d2l-input-checkbox>
 				</d2l-td>
 
 				<d2l-th scope="row" role="rowheader" class="userfullname">
-				  [[item.FirstName]] [[item.LastName]]
+				  [[item.DisplayName]] 
 				</d2l-th>
 
 				<d2l-th scope="row" role="rowheader">
@@ -169,7 +169,7 @@ class D2LActivityExemptions extends mixinBehaviors(
 		  <d2l-button
 			class="toggle-exemption-buttons bottom-buttons"
 			role="button"
-			aria-label$="[[localize('ariabtnLoadMore')]]"
+			aria-label$="[[localize('ariaLoadMore')]]"
 			on-click="loadMore"
 		  >
 			[[localize('btnLoadMore')]]
@@ -248,6 +248,12 @@ class D2LActivityExemptions extends mixinBehaviors(
 			.forEach(element => {
 				element.checked = e.target.checked;
 			});
+	}
+
+	getCheckboxAriaLabel(isExempt, displayName) {
+		var labelText = isExempt ? 'ariaSelectExemptUser' : 'ariaSelectNotExemptUser';
+
+		return this.localize(labelText, 'fullName', displayName);
 	}
 
 	showSaveToast(isExempt, numChanged) {
